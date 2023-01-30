e4130ba2c3b3e0211950ec58488a0b9728999130
lis76 <rsavoskin@yandex.ru>
2023-01-30 14:56:52 +0300

    1

--- app/src/main/java/ru/synergy/filewriterrider/MainActivity.java
+++ app/src/main/java/ru/synergy/filewriterrider/MainActivity.java
@@ -7,11 +7,14 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     private final static String FILE_NAME = "content.txt";
 
-    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //Сохранение файла
+
 }
